ea58a992a799550da3190fe140d79ba1ca6d97d5
rgruevski <robgruevski@gmail.com>
2022-10-22 18:26:49 -0400

    9. Shared Resources

---
+++ Resources/SharedResources.cs
@@ -0,0 +1,8 @@
+namespace UdemyMAUIMobile.Resources;
+
+static class SharedResources
+{
+    public static readonly Color TextColor = Color.FromRgb(0, 0, 255);
+    public static readonly Color ColorDark = Color.FromRgb(20, 20, 20);
+    public static readonly Color ColorLight = Color.FromRgb(200, 200, 200);
+} 
